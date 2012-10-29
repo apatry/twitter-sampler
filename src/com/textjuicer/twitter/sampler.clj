293d6ff14a80b6dc75888cb4bfe22bfc9e199daf
Alexandre Patry <alexandre.patry@keatext.com>
2012-10-29 00:00:05 -0400

    Clean up sampler.clj

--- src/com/textjuicer/twitter/sampler.clj
+++ src/com/textjuicer/twitter/sampler.clj
@@ -12,15 +12,33 @@
   (:import com.textjuicer.twitter.protocols.AsyncStreamingCallback)
   (:gen-class))
 
+(defn- printlog
+  "Print logging messages on *err*"
+  [& args]
+  (binding [*out* *err*]
+    (apply println args)))
+
+(defn- printerr
+  "Print error messages on *err*"
+  [& args]
+  (binding [*out* *err*]
+    (apply println args)))
+
 (defn- on-tweet
   "Helper to create a callback to process a tweet."
   [f]
   (fn [r baos]
     (let
         [code (:code (ac/status r))]
-      ;; only valid response are processed
-      (when  (and (>= code 200) (< code 300))
-        (f (-> baos str (parse-string true)))))))
+      (cond
+       ;; only valid response are processed
+       (and (>= code 200) (< code 300)) (f (-> baos str (parse-string true)))
+
+       ;; errors should abort the stream
+       (>= code 400) :abort
+
+       ;; ignore other kind of messages
+       :else nil))))
 
 (defn stop-after
   "A callback returning :abort after n tweets are processed."
@@ -35,21 +53,12 @@
   [out]
   #(generate-stream % out))
 
-(defn- printlog
-  "Print logging messages on *err*"
-  [& args]
-  (binding [*out* *err*]
-    (apply println args)))
-
-(defn- printerr
-  "Print error messages on *err*"
-  [& args]
-  (binding [*out* *err*]
-    (apply println args)))
-
 (defn download-tweets
-  "Download tweets and pass them as arguments to all the supplied callbacks f. Tweets are downloaded until one callback returns :abort."
+  "Download tweets and pass them as arguments to all the supplied
+  callbacks f. Tweets are downloaded until one callback
+  returns :abort."
   [credentials & f]
+
   ;; the client must be close to ensure its thread-pool is freed
   (with-open [client (ac/create-client :request-timeout -1 :follow-redirect false)]
     (let
@@ -57,24 +66,15 @@
          ;; retweet, ...)
          tweet? #(:id %)
 
-         ;; call all callback and returns :abort when at least one of
-         ;; them returns :abort
+         ;; call all callbacks passed in parameters and return :abort
+         ;; when at least one of them returns :abort
          process-tweet #(when (tweet? %)
                           (some #{:abort} (doall ((apply juxt f) %))))
 
-         ;; this callback will store each tweet in "tweets"
          callback (AsyncStreamingCallback.
-                   (on-tweet #(process-tweet %))
-
-                   (fn [response]
-                     (binding [*out* *err*]
-                       (-> response
-                           (response-return-everything :to-json? false)
-                           println)))
-
-                   (fn [response throwable]
-                     (binding [*out* *err*]
-                       (exception-print response throwable))))
+                   (on-tweet process-tweet)
+                   #(printerr (response-return-everything % :to-json? false))
+                   #(binding [*out* *err*] (exception-print %1 %2)))
 
          ;; open the stream with twitter
          response (statuses-sample :oauth-creds credentials
@@ -95,7 +95,8 @@
              ["-n" "--size" "Number of tweets to download."
               :default 1000 :parse-fn #(Integer. %)])
         credentials (:credentials options)
-        size (:size options)]
+        size (:size options)
+        output (first args)]
 
     (cond
      (:help options)
@@ -106,17 +107,17 @@
      (not credentials)
      (printerr "Credentials are required.")
 
-     (empty? args)
-     (printerr "One argument is required.")
+     (nil? output)
+     (printerr "One output file is required.")
 
      (> (count args) 1)
      (printerr "Too many arguments.")
 
      :else
      (if-let [creds (read-credentials credentials)]
-       (with-open [out (writer (first args))]
+       (with-open [out (writer output)]
          (download-tweets creds
                           (write-json out)
                           (stop-after size)))
-       (printerr "Invalid credentials (" credentials ")"))))
+       (printerr "Invalid credentials"))))
   nil)
