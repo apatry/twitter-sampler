cdbda0df2208e9b9805838dc8b7cbcbd90d5c5d9
Alexandre Patry <alexandre.patry@nlpfu.com>
2014-02-19 21:28:36 -0500

    Bump clojure version to 1.5.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject twitter-sampler "1.0.0-SNAPSHOT"
   :description "Build a corpus of tweets using the sample streaming api."
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "4.0.3"]
                  [org.clojure/tools.cli "0.2.2"]
                  [twitter-api "0.6.11"]]
