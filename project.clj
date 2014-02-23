3862f81143c2d81326f85d9a1a74753d19313bc2
Alexandre Patry <alexandre.patry@nlpfu.com>
2014-02-22 22:25:41 -0500

    Configure the logger

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject twitter-sampler "1.0.0-SNAPSHOT"
   :description "Build a corpus of tweets using the sample streaming api."
   :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ch.qos.logback/logback-classic "1.1.1"]
                  [cheshire "4.0.3"]
                  [org.clojure/tools.cli "0.2.2"]
                  [twitter-api "0.6.11"]]
