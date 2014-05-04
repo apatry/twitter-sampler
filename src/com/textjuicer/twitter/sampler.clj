bb12ad1645005755c1760089f200cfa900a8152c
Alexandre Patry <alexandre.patry@nlpfu.com>
2014-05-04 01:55:40 -0400

    fixes #3 Does not assume each block received from twitter is valid json

--- src/com/textjuicer/twitter/sampler.clj
+++ src/com/textjuicer/twitter/sampler.clj
@@ -2,18 +2,18 @@
   (:use
    [clojure.java.io :only (reader writer)]
    [clojure.tools.cli :only (cli)]
-   [cheshire.core :only (generate-stream parse-string)]
    [com.textjuicer.twitter.credentials :only (read-credentials)]
    [twitter.callbacks.handlers :only (exception-print response-return-everything)]
    [twitter.api.streaming :only (statuses-sample)])
   (:require
    [clojure.edn :as edn]
    [clojure.string :as str]
+   [cheshire.core :as json]
    [http.async.client :as ac]
    [com.textjuicer.twitter.protocols])
   (:import 
    com.textjuicer.twitter.protocols.AsyncStreamingCallback
-   java.io.PushbackReader)
+   [java.io InputStreamReader PipedInputStream PipedOutputStream PushbackReader])
   (:gen-class))
 
 (defn- printlog
@@ -28,67 +28,45 @@
   (binding [*out* *err*]
     (apply println args)))
 
-(defn- on-tweet
-  "Helper to create a callback to process a tweet."
-  [f]
+(defn- write-tweet
+  "A callback writing tweets to out"
+  [out]
   (fn [r baos]
     (let
         [code (:code (ac/status r))]
       (cond
        ;; only valid response are processed
-       (and (>= code 200) (< code 300)) (f (-> baos str (parse-string true)))
+       (and (>= code 200) (< code 300)) (try 
+                                          (.write out (.toByteArray baos))
+                                          (catch java.io.InterruptedIOException e
+                                            ;; stream has been closed on the other side
+                                                  :abort))
 
        ;; errors should abort the stream
        (>= code 400) :abort
 
        ;; ignore other kind of messages
-       :else nil))))
-
-(defn stop-after
-  "A callback returning :abort after n tweets are processed."
-  [n]
-  (let [calls (atom 0)]
-    (fn [tweet]
-      (swap! calls inc)
-      (when (>= @calls n) :abort))))
-
-(defn write-json
-  "A callback encoding tweets as json on out"
-  [out]
-  #(generate-stream % out))
-
-(defn report-progress
-  "Reports the number of tweets that have been processed on *out*."
-  [n]
-  (let [progress (atom 0)]
-    (fn [_]
-      (swap! progress inc)
-      (print "\r" @progress "tweets")
-      (when (= @progress n)
-        (println)))))
+       :else (printerr "Received " ac/status " from server.")))))
 
 (defn download-tweets
   "Download tweets and pass them as arguments to all the supplied
   callbacks (list of functions). Tweets are downloaded until one callback
   returns :abort."
-  [credentials callbacks & {:keys [proxy]}]
+  [credentials size out & {:keys [proxy]}]
 
   ;; the client must be close to ensure its thread-pool is freed
-  (with-open [client (ac/create-client :request-timeout -1 
+  (with-open [twitter-out (PipedOutputStream.)
+              in (InputStreamReader. (PipedInputStream. twitter-out) "UTF-8")
+              client (ac/create-client :request-timeout -1 
                                        :follow-redirect false
                                        :proxy proxy)]
     (let
         [;; identify original tweet (returns false for deleted tweet,
          ;; retweet, ...)
-         tweet? #(:id %)
-
-         ;; call all callbacks passed in parameters and return :abort
-         ;; when at least one of them returns :abort
-         process-tweet #(when (tweet? %)
-                          (some #{:abort} (doall ((apply juxt callbacks) %))))
+         tweet? #("id" %)
 
          callback (AsyncStreamingCallback.
-                   (on-tweet process-tweet)
+                   (write-tweet twitter-out)
                    #(printerr (response-return-everything % :to-json? false))
                    #(binding [*out* *err*] (exception-print %1 %2)))
 
@@ -98,7 +76,10 @@
                                    :client client)]
 
       ;; wait until one callback returns :abort and then close the stream
-      (ac/await response))))
+      (doseq [[i tweet] (map-indexed vector (take size (filter tweet? (json/parsed-seq in))))]
+        (print (inc i) " tweets\r")
+        (json/generate-stream tweet out))
+      (println))))
 
 (defn read-edn
   "Read an edn datastructure from a file."
@@ -144,17 +125,6 @@
          (when proxy
            (println "Downloading using proxy " proxy)
            (println "Invalid proxy parameters may make the connection hangs."))
-         (let
-             [counter (atom 0)]
-           (while (< @counter size)
-             (when (> @counter 0)
-               (println "Connection closed after " @counter " tweets, resuming in 10 seconds...")
-               (Thread/sleep 10000))
-             (download-tweets creds
-                              [(write-json out)
-                               (report-progress size)
-                               (stop-after (- size @counter))
-                               (fn [tweet] (swap! counter inc))]
-                              :proxy proxy))))
+         (download-tweets creds size out :proxy proxy))
        (printerr "Invalid credentials"))))
   nil)
