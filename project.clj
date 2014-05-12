1210345495ef44939af1c850978a3ae2ca7ed869
Alexandre Patry <alexandre.patry@nlpfu.com>
2014-05-11 22:53:58 -0400

    Add url and license to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,10 @@
 (defproject twitter-sampler "1.0.0-SNAPSHOT"
-  :description "Build a corpus of tweets using the sample streaming api."
+  :description "Build a corpus of tweets using twitter streaming api."
+  :url "https://github.com/apatry/twitter-sampler"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ch.qos.logback/logback-classic "1.1.1"]
                  [cheshire "4.0.3"]
